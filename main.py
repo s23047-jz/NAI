fa81c405c6953efe6c311a6df443c96a3fa3d48d
Matieus <76398414+Matieus@users.noreply.github.com>
2023-09-30 14:59:00 +0200

    Update main.py
    Add docs

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ until the second player, with red discs, achieves a diagonal four in a row, and 
 If the board fills up before either player achieves four in a row, then the game is a draw.
 """
 
+
+"""
+PopOut
+"PopOut" redirects here. For other uses, see Pop Out (disambiguation).
+
+A travel version of the Milton Bradley game
+PopOut starts the same as traditional gameplay, 
+with an empty board and players alternating turns placing their own colored discs into the board.
+During each turn, a player can either add another disc from the top,
+or if one has any discs of their own color on the bottom row,
+remove (or "pop out") a disc of one's own color from the bottom.
+Popping a disc out from the bottom drops every disc above it down one space, 
+changing their relationship with the rest of the board and changing the possibilities for a connection. 
+The first player to connect four of their discs horizontally, vertically, or diagonally wins the game.
+"""
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
