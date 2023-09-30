31f818c3264adb217ae6c894298fb2b17cad6c0d
s23047-jz <73025973+s23047-jz@users.noreply.github.com>
2023-09-30 15:13:06 +0200

    created classes

---
+++ classes/game.py
@@ -0,0 +1,11 @@
+class Game:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+    def _valid_data(self):
+        pass
+
+    def _create_board(self):
+        pass
---
+++ classes/player.py
@@ -0,0 +1,16 @@
+class Player:
+    def __init__(self, name, coins):
+        self.name = name
+        self.coins = coins
+
+    def _remove_coin(self):
+        self.coins -= 1
+
+    def has_player_coins(self):
+        return self.coins > 0
+
+    def get_player_name(self):
+        return self.name
+
+    def get_number_of_coins(self):
+        return self.coins
