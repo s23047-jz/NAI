fb109f3a28e5dc184427bd86b56b38c20326d9ea
s23047-jz <73025973+s23047-jz@users.noreply.github.com>
2023-10-06 19:57:44 +0200

    implemented new methods

--- classes/game.py
+++ classes/game.py
@@ -1,27 +1,49 @@
 import numpy as np
-from typing import Any
+from easyAI import TwoPlayerGame, Negamax
+from easyAI.games import ConnectFour
 
 
-class Game:
-    def __init__(self, x: int, y: int):
-        self._valid_data(x, y)
-        self.x = x
-        self.y = y
-        self.board = self._create_board()
+class Game(TwoPlayerGame):
+    def __init__(self, players):
+        self.rows = 7
+        self.cols = 6
+        self.players = players
         self.move_counter = 0
-
-    def _valid_data(self, x: Any, y: Any):
-        if not isinstance(x, int) or not isinstance(y, int):
-            raise ValueError("Error, the values are not integer")
+        self.board = self._create_board()
 
     def _create_board(self):
-        return np.zeros((self.x, self.y), dtype=str)
+        return np.zeros((self.rows, self.cols), dtype=str)
 
-    def _valid_move(self, row):
-        return self.board[row, 0] == ''
+    def _find_four(self):
+        for row, col in self.board:
+            streak = 0
+            while (0 <= row[0] <= 5) and (0 <= row[1] <= 6):
+                # TODO implement current player char
+                if self.board[row[0], row[1]] == self._get_current_player_character(2):
+                    streak += 1
+                    if streak == 4:
+                        return True
+                else:
+                    streak = 0
+                row = row + col
+        return False
 
-    def place_piece(self, row: int, column: int):
-        self.board[row, column] = 'x'
+    def _get_current_player_character(self, player_index):
+        return 'O' if player_index == 2 else 'X'
 
-    def show_board(self):
+    def possible_moves(self):
+        return [c for c in range(self.rows) if self.board[0][c] == '']
+
+    def make_move(self, column):
+        row = max([r for r in range(self.cols) if self.board[r][column] == ''])
+        # TODO implement current player char
+        self.board[row][column] = self._get_current_player_character(2)
+
+    def lose(self):
+        return self._find_four()
+
+    def is_over(self):
+        return len(self.possible_moves()) == 0 or self.lose()
+
+    def show(self):
         print(self.board)
