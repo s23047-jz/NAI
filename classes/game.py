a2af3173c57627eb3fc8a0398a70fec4502f2b9a
s23047-jz <73025973+s23047-jz@users.noreply.github.com>
2023-10-06 20:14:16 +0200

    implemented game start

---
+++ classes/game.py
@@ -0,0 +1,47 @@
+import numpy as np
+from easyAI import TwoPlayerGame
+from easyAI.games import ConnectFour
+
+
+class Game(TwoPlayerGame):
+    def __init__(self, players):
+        self.rows = 7
+        self.cols = 6
+        self.players = players
+        self.move_counter = 0
+        self.board = self._create_board()
+
+    def _create_board(self):
+        return np.zeros((self.rows, self.cols), dtype=str)
+
+    def _find_four(self):
+        for row, col in self.board:
+            streak = 0
+            while (0 <= row[0] <= 5) and (0 <= row[1] <= 6):
+                if self.board[row[0], row[1]] == self._get_current_player_character(self.opponent_index):
+                    streak += 1
+                    if streak == 4:
+                        return True
+                else:
+                    streak = 0
+                row = row + col
+        return False
+
+    def _get_current_player_character(self, player_index):
+        return 'O' if player_index == 2 else 'X'
+
+    def possible_moves(self):
+        return [c for c in range(self.rows) if self.board[0][c] == '']
+
+    def make_move(self, column):
+        row = max([r for r in range(self.cols) if self.board[r][column] == ''])
+        self.board[row][column] = self._get_current_player_character(self.opponent_index)
+
+    def lose(self):
+        return self._find_four()
+
+    def is_over(self):
+        return len(self.possible_moves()) == 0 or self.lose()
+
+    def show(self):
+        print(self.board)
--- main.py
+++ main.py
@@ -8,16 +8,33 @@ Rules:
     variant: PopOut
 """
 
+from easyAI import AI_Player, Human_Player, Negamax, SSS
 
 from classes.game import Game
 
 
 def main():
-    game = Game(6, 7)
-    game.show_board()
-    print("===============")
-    game.place_piece(2, 3)
-    game.show_board()
+    player_choice = ''
+    available_choice = ['player', 'ai']
+    while (player_choice == '' or player_choice not in available_choice):
+        player_choice = input("Please select game type, 'player' to play against AI, or 'ai' to watch game between two AIs")
+
+    neg_ai = Negamax(5)
+    sss_ai = SSS(5)
+
+    if player_choice == 'player':
+        player_1 = Human_Player
+        player_2 = AI_Player(neg_ai)
+    elif player_choice == 'ai':
+        player_1 = AI_Player(neg_ai)
+        player_2 = AI_Player(sss_ai)
+
+    game = Game([player_1, player_2])
+    game.play()
+    if game.lose():
+        print("Player %d wins." % (game.opponent_index))
+    else:
+        print("Looks like we have a draw.")
 
 
 if __name__ == '__main__':
