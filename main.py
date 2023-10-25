32a0c6c07277998e77b76c7cc5d1e49e3b3f29ee
Matieus <76398414+Matieus@users.noreply.github.com>
2023-10-25 18:01:57 +0200

    Edited docs in main

--- main.py
+++ main.py
@@ -24,15 +24,20 @@ from classes.city import City, create_dict_for_city
 def main():
     city = City(get_data_from_api=True, city_index=0)
     city.initialise()
-    # city = City()
-    # city.initialise(create_dict_for_city(
-    #     name="Test",
-    #     pm10=1.0,
-    #     pm25=1.0,
-    #     o3=1.0,
-    #     no2=1.0,
-    #     so2=1.0
-    # ))
+    """
+    Example of use City with custom data,
+    without connecting with apowietrze.gios.gov.pl api 
+    
+    city = City()
+    city.initialise(create_dict_for_city(
+        name="Test",
+        pm10=1.0,
+        pm25=1.0,
+        o3=1.0,
+        no2=1.0,
+        so2=1.0
+    ))
+    """
     city.show()
     city_air_pollution = CityAirQuality(city=city)
     city_air_pollution.show()
