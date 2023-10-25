93e7172a66ad8daa0ddb51580febfeb7b48b8242
s23047-jz <73025973+s23047-jz@users.noreply.github.com>
2023-10-25 19:52:47 +0200

    updated main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Authors:
-    Jakub Żurawski: https://github.com/s23047-jz/NAI/tree/air-pollution
+    Jakub Żurawski: https://github.com/s23047-jz/NAI/
     Mateusz Olstowski: https://github.com/Matieus/NAI/tree/air-pollution
 
 used api: https://powietrze.gios.gov.pl/pjp/current#
@@ -22,22 +22,17 @@ from classes.city import City, create_dict_for_city
 
 
 def main():
-    city = City(get_data_from_api=True, city_index=0)
-    city.initialise()
-    """
-    Example of use City with custom data,
-    without connecting with apowietrze.gios.gov.pl api 
-    
+    # city = City(get_data_from_api=True, city_index=0)
+    # city.initialise()
     city = City()
     city.initialise(create_dict_for_city(
         name="Test",
-        pm10=1.0,
-        pm25=1.0,
-        o3=1.0,
-        no2=1.0,
-        so2=1.0
+        pm10=47.00,
+        pm25=45.0,
+        o3=0.1,
+        no2=0.1,
+        so2=0.1
     ))
-    """
     city.show()
     city_air_pollution = CityAirQuality(city=city)
     city_air_pollution.show()
