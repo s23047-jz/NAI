534780f10ba4578cf9ab68e3c501680406855d24
s23047-jz <s23047@pjwstk.edu.pl>
2024-01-05 23:09:44 +0100

    implemented reduce and increase sound level for media player

--- main.py
+++ main.py
@@ -6,13 +6,49 @@ Authors:
 ---
 """
 
-import os
+import time
 import platform
 import subprocess
 
 import cv2 as cv
 import mediapipe as mp
 import numpy as np
+from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
+
+
+class MediaPlayer:
+    def __init__(self):
+        self.process_name = "Microsoft.Media.Player.exe"
+        self.increase_reduce = 0.1
+        self.volume_interface = self._get_volume_interface()
+        self._valid_volume_interface()
+
+    def _get_volume_interface(self):
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            if session.Process and session.Process.name() == self.process_name:
+                return session._ctl.QueryInterface(ISimpleAudioVolume)
+        return None
+
+    def _valid_volume_interface(self):
+        if self.volume_interface == None or self.volume_interface == '':
+            raise Exception("Couldn't find an app")
+
+    def is_media_player_playing(self):
+        if self.volume_interface:
+            return self.volume_interface.GetMute()
+
+    def reduce_volume(self):
+        if self.volume_interface:
+            current_volume = self.volume_interface.GetMasterVolume()
+            new_volume = max(0.0, current_volume - self.increase_reduce)
+            self.volume_interface.SetMasterVolume(new_volume, None)
+
+    def increase_volume(self):
+        if self.volume_interface:
+            current_volume = self.volume_interface.GetMasterVolume()
+            new_volume = min(1.0, current_volume + self.increase_reduce)
+            self.volume_interface.SetMasterVolume(new_volume, None)
 
 
 class Celluloid:
@@ -39,11 +75,9 @@ class Celluloid:
             raise Exception("Couldn't find an app")
 
     def reduce_volume(self):
-        print('ID', self.id)
         subprocess.run(['pactl', 'set-sink-volume', self.id, '-10%'])
 
     def increase_volume(self):
-        print('ID', self.id)
         subprocess.run(['pactl', 'set-sink-volume', self.id, '+10%'])
 
 
@@ -55,13 +89,25 @@ def main():
             celluloid = Celluloid()
             command = ''
             while command != 'q':
-                command = input("Enter '-' if you want to reduce the sound level or '+' if increase ('q' = quit): ")
+                command = input(f"[{system.upper()}] Enter '-' to reduce the sound level, '+' to increase or 'q' "
+                                f"to quit: ")
                 if command == '-':
                     celluloid.reduce_volume()
                 elif command == '+':
                     celluloid.increase_volume()
         case 'windows':
-            pass
+            media_player = MediaPlayer()
+            command = ''
+            while command != 'q':
+                command = input(f"[{system.upper()}] Enter '-' to reduce the sound level, '+' to increase, '!' to "
+                                f"pause or resume, 'q' to quit: ")
+                if command == '-':
+                    media_player.reduce_volume()
+                elif command == '+':
+                    media_player.increase_volume()
+                elif command == '!':
+                    if media_player.is_media_player_playing():
+                        pass
         case _:
             raise Exception("Unsupported os!")
 
